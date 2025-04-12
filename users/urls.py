67e2038c55301e3547441aeb7930c38cffa69d82
Sohaib-Azhar010 <sohaibazhar04@gmail.com>
2025-04-12 20:31:22 +0500

    frontend done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
-from .views import hello_view
+from .views import (
+    hello_view,
+    about_view,
+    contact_view,
+    blog_list_view,
+    testimonials_view,
+    product_view,
+    description_view,
+    cart_view,
+    checkout_view
+)
+
 
 urlpatterns = [
-    path('', hello_view, name='hello'),
+    path('', hello_view, name='home'),
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
+    path('blog/', blog_list_view, name='blog'),
+    path('testimonials/', testimonials_view, name='testimonial'),
+    path('products/', product_view, name='products'),
+    path('description/', description_view, name='description'),
+    path('cart/', cart_view, name='cart'),
+    path('checkout/', checkout_view, name='checkout'),
 ]
+
--- users/views.py
+++ users/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
 def hello_view(request):
-    return render(request, 'demo.html')
+    return render(request, 'index.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def blog_list_view(request):
+    return render(request, 'blog_list.html')
+
+def testimonials_view(request):
+    return render(request, 'testimonial.html')
+
+def product_view(request):
+    return render(request, 'product.html')
+
+def description_view(request):
+    return render(request, 'description.html')
+
+def cart_view(request):
+    return render(request, 'cart.html')
+
+def checkout_view(request):
+    return render(request, 'checkout.html')
