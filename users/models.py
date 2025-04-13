81b505af41d136d29654b19aef195d46f7e1f014
abdullah956 <abdullaharshed956@gmail.com>
2025-04-13 23:36:12 +0500

    contact model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from config.models import BasedModel
 
-# Create your models here.
+class ContactMessage(BasedModel):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - {self.subject}"
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactMessageForm
 
 def home_view(request):
     return render(request, 'home.html')
@@ -8,7 +8,11 @@ def about_view(request):
     return render(request, 'users/about.html')
 
 def contact_view(request):
-    return render(request, 'users/contact.html')
+    form = ContactMessageForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('contact')
+    return render(request, 'users/contact.html', {'form': form})
 
 def testimonials_view(request):
     return render(request, 'users/testimonial.html')
