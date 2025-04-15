f07d7eee594839c391642fa9a81697acaa664eb5
abdullah956 <abdullaharshed956@gmail.com>
2025-04-15 13:31:46 +0500

    reviews

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import ContactMessageForm,NewsletterSubscriberForm
 from django.contrib import messages
-from products.models import Product
+from products.models import Product, Review
+from django.db.models import Count
 
 def home_view(request):
     products = Product.objects.all()
@@ -13,7 +14,22 @@ def home_view(request):
             form.save()
             return redirect(request.path)
 
-    return render(request, 'home.html', {'products': products, 'form': form})
+    reviews = (
+        Review.objects.filter(rating=5)
+        .select_related('product')
+        .values('product')
+        .annotate(count=Count('product'))
+        .order_by('-count')[:3]
+    )
+
+    product_ids = [r['product'] for r in reviews]
+    testimonials = Review.objects.filter(product__in=product_ids, rating=5).select_related('product')[:9]
+
+    return render(request, 'home.html', {
+        'products': products,
+        'form': form,
+        'testimonials': testimonials
+    })
 
 
 def about_view(request):
@@ -27,7 +43,18 @@ def contact_view(request):
     return render(request, 'users/contact.html', {'form': form})
 
 def testimonials_view(request):
-    return render(request, 'users/testimonial.html')
+    reviews = (
+        Review.objects.filter(rating=5)
+        .select_related('product')
+        .values('product')
+        .annotate(count=Count('product'))
+        .order_by('-count')[:3]
+    )
+
+    product_ids = [r['product'] for r in reviews]
+
+    testimonials = Review.objects.filter(product__in=product_ids, rating=5).select_related('product')[:9]
+    return render(request, 'users/testimonial.html', {'testimonials': testimonials})
 
 def newsletter_signup(request):
     if request.method == 'POST':
